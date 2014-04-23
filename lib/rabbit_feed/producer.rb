c94003a13d8ee55e0412f981c913ac86a053622d
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2014-04-23 09:31:19 +0100

    return the event after publish

--- lib/rabbit_feed/producer.rb
+++ lib/rabbit_feed/producer.rb
@@ -4,6 +4,7 @@ module RabbitFeed
     def self.publish_event name, payload
       event = Event.new RabbitFeed.configuration.application, RabbitFeed.configuration.version, name, payload
       ProducerConnection.publish event.serialize, event.routing_key
+      event
     end
   end
 end
