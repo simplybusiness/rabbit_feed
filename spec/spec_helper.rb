f09d4d0e45fd7fcc39476584163ea88957c0891b
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2014-05-05 16:22:06 +0100

    add dummy airbrake configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,11 @@ I18n.enforce_available_locales = true
 # Loads the step definitions
 Dir.glob('spec/features/step_definitions/**/*_steps.rb') { |f| load f, true }
 
+# Dummy airbrake config
+Airbrake.configure do |config|
+  config.api_key = 'blah'
+end
+
 RSpec.configure do |config|
 
   config.before do
