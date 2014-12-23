dacfef288b2a8fc13326688ab9c382625848ae61
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2014-12-23 13:21:31 +0000

    Upgrade example projects
    Upgrade to rails 4.2 and rspec 3.1

--- example/non_rails_app/spec/lib/non_rails_app/event_routing_spec.rb
+++ example/non_rails_app/spec/lib/non_rails_app/event_routing_spec.rb
@@ -5,7 +5,7 @@ module NonRailsApp
     let(:event) { {'application' => 'rails_app', 'name' => 'user_creates_beaver'} }
 
     it 'routes events correctly' do
-      expect(NonRailsApp::EventHandler).to receive(:handle_event).with { |full_event| expect(full_event.payload).to eq(event)}
+      expect(NonRailsApp::EventHandler).to receive(:handle_event) { |full_event| expect(full_event.payload).to eq(event)}
       rabbit_feed_consumer.consume_event(event)
     end
   end
