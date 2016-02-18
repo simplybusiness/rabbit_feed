ac08bfa8077c5180d88fd2929c720f2fd5f85a64
PeterWuMC <peter.wu@simplybusiness.co.uk>
2016-02-18 08:45:52 +0000

    Add some explanation on the why

--- lib/rabbit_feed.rb
+++ lib/rabbit_feed.rb
@@ -38,8 +38,11 @@ module RabbitFeed
         (Airbrake.notify_or_ignore exception) if Airbrake.configuration.public?
       elsif defined?(Airbrake::AIRBRAKE_VERSION) && Airbrake::AIRBRAKE_VERSION.to_i >= 5
         if RabbitFeed.configuration.consumer_exit_after_fail
+          # Will need to send the notification right away, otherwise the `exit` would kill the
+          # Airbrake before the notification is sent out
           Airbrake.notify_sync exception
         else
+          # Airbrake notify default to sending notification asynchronously
           Airbrake.notify exception
         end
       end
